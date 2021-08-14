ee9ff6d0b246bedf4f6d6d781f3390211042153c
AdityaChoudhary5 <aditya20489@iiitd.ac.in>
2021-08-14 15:23:15 +0530

    Patch 1.1

--- Simple-Assembler/src/main.py
+++ Simple-Assembler/src/main.py
@@ -26,7 +26,6 @@ def convertion(instr,temp_var):
 
 
 #/////////////////
-
 def check_inst(str):
     w = str.split(" ")
     if(w[0]=='mov'):
@@ -38,25 +37,32 @@ def check_inst(str):
     else:
         type = IS.opcode_table[w[0]][2]
     if(IS.type_check(str,type)):
-        IS.execute(str,type)
+        IS.execute(str)
+        print(IS.binary(str,type))
         return(True)
     return(False)
 
-# add\tr1 r2 r3 -> add r1 r2 r3
 
 def check_line(line):
     line = re.sub(r'(\\[a-zA-Z])+'," ",line)
     w = line.split(" ")
-    if(re.match("[a-zA-z]+: ",line)):
-        check_inst(" ".join(w[1:]))
+    if(re.match("[a-zA-z]+: ([a-zA-Z0-9]+ )+",line)):
+        if(not check_inst(" ".join(w[1:]))):
+            print(" Error \n")
+        else:
+            if(w[1]!="cmp" and w[1] in IS.opcode_table):
+                IS.registers["FLAGS"] = '0000000000000000'
+            return(1)
     elif(re.match("\Avar [a-zA-Z]+",line)):
         return(3)
     elif(w[0] in IS.opcode_table):
         if(not check_inst(line)):
             print(" Error \n")
         else:
+            if(w[0]!="cmp"):
+                IS.registers["FLAGS"] = '0000000000000000'
             return(2)
-    else: # " " or ""
+    else:
         return (4)
 
 def main():
@@ -68,13 +74,9 @@ def main():
             hlt_count = 0
             line_type = check_line(line)
             if(line_type!=4):
-                print(line+" ", line_type)    
-            IS.registers["FLAGS"] = '0000000000000000'
-            #for read in line.split(" "):
-             #   ISA.__init__()
-              #  ISA.type_check(read)
+                print(line+" ", line_type)
 
-    
+
 if __name__ == "__main__":
     IS = ISA16bit()
     IS.__init__()
